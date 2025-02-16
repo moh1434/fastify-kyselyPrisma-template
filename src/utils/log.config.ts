7dca7e8cc1ad791fabc7a66b194f9e4381b9c574
moh1434 <moh1434.ma@gmail.com>
2025-02-16 23:51:14 +0300

    remove not needed data

--- src/utils/log.config.ts
+++ src/utils/log.config.ts
@@ -4,13 +4,13 @@ import { PinoLoggerOptions } from "fastify/types/logger.js";
 export function loggingConfig(): FastifyLoggerOptions & PinoLoggerOptions {
   return {
     transport:
-      process.env.ENABLE_FASTIFY_LOGGER === "1"
+      Number(process.env.ENABLE_FASTIFY_LOGGER) === 1
         ? {
             target: "pino-pretty",
             options: {
               colorize: true,
               translateTime: "HH:MM:ss Z",
-              ignore: "pid,hostname,reqId,err", // Hide unnecessary fields
+              ignore: "level,time,pid,hostname,reqId", // Hide unnecessary fields
               messageFormat: "{msg}",
             },
           }
